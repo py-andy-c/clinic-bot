f193e31f88ef7fbf9bf2991188aabb9e3b8842d6
Andy19961017 <pychen1017@gmail.com>
2025-11-11 07:45:46 -0800

    wording

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -50,7 +50,7 @@ const LandingPage: React.FC = () => {
               線上諮詢
             </h2>
             <p className="text-base sm:text-lg text-gray-600 max-w-2xl mx-auto">
-              24小時提供專業醫療諮詢，協助病患了解症狀與治療選項
+              24小時提供專業醫療諮詢，服務病患也抓住潛在客源
             </p>
           </div>
 
