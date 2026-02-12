3cc41e3ea188b23818c40863b4605a496bf27aab
Andy19961017 <pychen1017@gmail.com>
2026-02-11 23:40:12 -0800

    Rename tab

--- frontend/src/pages/PatientDetailPage.tsx
+++ frontend/src/pages/PatientDetailPage.tsx
@@ -250,7 +250,7 @@ const PatientDetailPage: React.FC = () => {
               : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               }`}
           >
-            病歷記錄
+            病歷與表單
           </button>
           <button
             onClick={() => setActiveTab('photos')}
